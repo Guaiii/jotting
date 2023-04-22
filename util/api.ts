a17d4aa2629489b7c53b253aa14496c65a21d310
tanwenxin <xindi2570@163.com>
2023-04-22 14:59:04 +0800

    删除log

--- util/api.ts
+++ util/api.ts
@@ -1,15 +1,14 @@
-
 const isDev = process.env.NODE_ENV === 'development'
-console.log({isDev});
-export let BASE_URL = isDev ? 'http://localhost:3000/api' : '/api'
 
+export let BASE_URL = isDev ? 'http://localhost:3000/api' : 'jotting-delta.vercel.app/api'
+console.log({isDev, BASE_URL});
 
-export const validValue = (obj:Record<string,any>):boolean => {
-  if(!obj || typeof obj !== 'object'){
-      return false
-  }
-    for (const [k,v] of Object.entries(obj)) {
-        if(!v && k !== 'important'){
+export const validValue = (obj: Record<string, any>): boolean => {
+    if (!obj || typeof obj !== 'object') {
+        return false
+    }
+    for (const [k, v] of Object.entries(obj)) {
+        if (!v && k !== 'important') {
             return false
         }
     }
