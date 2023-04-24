0d0ed03fac39d9c68d0b5fb10b90f2b29f631807
tanwenxin <xindi2570@163.com>
2023-04-24 09:15:46 +0800

    feat: base

--- util/api.ts
+++ util/api.ts
@@ -1,6 +1,6 @@
 const isDev = process.env.NODE_ENV === 'development'
 
-export let BASE_URL = isDev ? 'http://localhost:3000/api' : '/api'
+export let BASE_URL = isDev ? process.env.BASE_URL_DEV : process.env.BASE_URL_PROD
 console.log({isDev, BASE_URL});
 
 export const validValue = (obj: Record<string, any>): boolean => {
